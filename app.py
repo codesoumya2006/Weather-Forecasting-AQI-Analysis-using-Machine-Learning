53f27b2284bf75b6f211bd16773c747add5f6b07
Soumyadeep Das Adhikary <soumyadeepdas242006@gmail.com>
2026-01-19 20:52:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import json
 import requests
+import joblib
 from datetime import datetime, timedelta
 import plotly.graph_objects as go
 import plotly.express as px
